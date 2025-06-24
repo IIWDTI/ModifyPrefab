6790c1fe4b4be168bf20e23bf9de694cb2612cc0
IIWDTI <60497271+IIWDTI@users.noreply.github.com>
2025-06-24 13:38:57 +0200

    Create AssemblyInfo.cs

---
+++ 2.0.0.1/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ModifyPrefab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModifyPrefab")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fb71e33c-a864-47a4-a7e3-9096d039f33e")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.1")]
